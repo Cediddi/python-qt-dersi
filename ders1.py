50856d52fc723b44244c1d4cefffff8888d02e1c
Umut Karcı <cediddi@gmail.com>
2019-02-08 21:53:54 +0300

    Sana Da Merhaba Ey Yazar

--- ders1.py
+++ ders1.py
@@ -1,8 +1,16 @@
-from PySide2 import QtWidgets
+from PySide2 import QtCore, QtWidgets
+
 app = QtWidgets.QApplication()
 
 label = QtWidgets.QLabel()
 label.setText("Merhaba Dünya")
 label.show()
+
+button = QtWidgets.QPushButton()
+button.setText("Sana Da Merhaba Ey Yazar")
+button.clicked.connect(app.quit)
+
+timer = QtCore.QTimer()
+timer.singleShot(1000, lambda: (button.show(), label.hide()))
 
 app.exec_()
