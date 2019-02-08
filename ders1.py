1aa5d6c6740a81f87295c56d209be5516e06d860
Umut Karcı <cediddi@gmail.com>
2019-02-08 22:24:15 +0300

    Pencereler 1.0: Yeni Bir Kod

--- ders1.py
+++ ders1.py
@@ -1,22 +1,29 @@
-from PySide2 import QtCore, QtWidgets
+from PySide2 import QtWidgets
 
 app = QtWidgets.QApplication()
+window = QtWidgets.QWidget()
 
-text_input = QtWidgets.QLineEdit()
+text_input = QtWidgets.QLineEdit(window)
 text_input.setPlaceholderText("Adınız?")
-text_input.show()
 
-button = QtWidgets.QPushButton()
+button = QtWidgets.QPushButton(window)
 button.clicked.connect(app.quit)
+button.setDisabled(True)
 
 
 def button_callback():
     text = text_input.text()
     button.setText(f"Sana Da Merhaba Ey {text}")
-    text_input.hide()
-    button.show()
+    text_input.setDisabled(True)
+    button.setDisabled(False)
 
+
+main_layout = QtWidgets.QVBoxLayout(window)
+main_layout.addWidget(text_input)
+main_layout.addWidget(button)
 
 text_input.returnPressed.connect(button_callback)
 
+window.setLayout(main_layout)
+window.show()
 app.exec_()
