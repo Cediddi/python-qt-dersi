b2be80d32593e35e38ea3f1bce8c6a525500a11c
Umut Karcı <cediddi@gmail.com>
2019-02-08 23:11:30 +0300

    Pencereler 1.2: Reset'in Dönüşü

--- ders1.py
+++ ders1.py
@@ -9,33 +9,51 @@ class MainWindow(QtWidgets.QMainWindow):
         self.build_layout()
         self.build_events()
 
-    def button_callback(self):
+    def return_pressed_callback(self):
         text = self.text_input.text()
         self.button.setText(f"Sana Da Merhaba Ey {text}")
         self.text_input.setDisabled(True)
         self.button.setDisabled(False)
+
+    def button_clicked_callback(self):
+        self.button.setDisabled(True)
+        self.button.setText("Merhaba Dünya")
+        self.text_input.clear()
+
+    def reset_clicked_callback(self):
+        self.text_input.setDisabled(False)
+        self.text_input.clear()
+        self.button.setDisabled(True)
+        self.button.setText("---")
 
     def build_widgets(self):
         self.text_input = QtWidgets.QLineEdit(self.centralWidget())
         self.text_input.setPlaceholderText("Adınız?")
 
         self.button = QtWidgets.QPushButton(self.centralWidget())
+        self.button.setText("---")
         self.button.setDisabled(True)
+
+        self.reset_button = QtWidgets.QPushButton(self.centralWidget())
+        self.reset_button.setText("Reset")
 
     def build_layout(self):
         self.main_layout = QtWidgets.QVBoxLayout()
         self.main_layout.addWidget(self.text_input)
         self.main_layout.addWidget(self.button)
+        self.main_layout.addWidget(self.reset_button)
 
         self.centralWidget().setLayout(self.main_layout)
 
     def build_events(self):
-        self.text_input.returnPressed.connect(self.button_callback)
-        self.button.clicked.connect(app.quit)
+        self.text_input.returnPressed.connect(self.return_pressed_callback)
+        self.button.clicked.connect(self.button_clicked_callback)
+        self.reset_button.clicked.connect(self.reset_clicked_callback)
 
 
-app = QtWidgets.QApplication()
-window = MainWindow()
-window.build()
-window.show()
-app.exec_()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication()
+    window = MainWindow()
+    window.build()
+    window.show()
+    app.exec_()
