47f40be3efbad3bb88a6d9c471abae1f66684026
Umut Karcı <cediddi@gmail.com>
2019-02-10 16:18:38 +0300

    Pencereler 2.1: Urllib'in İntikamı

---
+++ data_conn.py
@@ -0,0 +1,12 @@
+import json
+from urllib import request
+url = "http://51.15.221.223:8080/"
+
+def mesaj_gonder(isim, mesaj):
+    data = json.dumps({"message":mesaj, "sender":isim}).encode()
+    rq = request.Request(url, data, headers={'content-type': 'application/json'})
+    response = request.urlopen(rq)
+    return json.loads(response.read())
+
+def mesaj_al():
+    return json.loads(request.urlopen(url).read())
